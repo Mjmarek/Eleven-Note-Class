9fef029cb549445dcd3312e525be687fff9f203c
Mjmarek <monicamarek24@gmail.com>
2017-11-15 14:33:32 -0500

    Connected controller to services

---
+++ ElevenNote.Services/NoteService.cs
@@ -0,0 +1,34 @@
+﻿using ElevenNote.Data;
+using ElevenNote.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevenNote.Services
+{
+    public class NoteService
+    {
+        public IEnumerable<NoteListItemModel> GetNotes()
+        {
+            using (var ctx = new ElevenNoteDbContext()) //cleans up database connections
+            {
+                return
+                    ctx
+                        .Notes
+                        .Select(//transforms NoteEntity into another shape so controller can understand it
+                            e => //"e" is individual NoteEntity from Db
+                                new NoteListItemModel
+                                {
+                                    NoteId = e.NoteId,
+                                    Title = e.Title,
+                                    CreatedUtc = e.CreatedUtc,
+                                    ModifiedUtc = e.ModifiedUtc
+                                })
+                        .ToArray();//load all data to array
+
+            }
+        }
+    }
+}
---
+++ ElevenNote.Web/Controllers/NotesController.cs
@@ -0,0 +1,22 @@
+﻿using ElevenNote.Models;
+using ElevenNote.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ElevenNote.Web.Controllers
+{
+    [Authorize]//visitor to website needs to be logged in to access content below
+    public class NotesController : Controller
+    {
+        // GET: Notes
+        public ActionResult Index()
+        {
+            var svc = new NoteService();
+            var model = svc.GetNotes();
+            return View(model);
+        }
+    }
+}
